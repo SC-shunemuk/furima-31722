996f053ce731e33b8a3d0350a440756f9a3532e4
SC-shunemuk <go.shun.ku@gmail.com>
2020-11-18 15:32:48 +0900

    with_optionsを使ったバリデーションの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_date, presence: true
+  with_options presence: true do
+    
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+      validates :first_name
+      validates :last_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+
+    validates :birth_date
+    validates :nickname
+  
+  end
+  
   validates :password, format: { with: /[a-zA-Z]+[0-9]/ }
 end
