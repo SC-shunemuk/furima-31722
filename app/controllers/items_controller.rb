9edf496bd27bd418b31fd31c04c75a3807ef2603
SC-shunemuk <go.shun.ku@gmail.com>
2020-11-27 18:45:15 +0900

    削除機能の条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,8 +22,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    @item.destroy 
+    redirect_to root_path if @item.user_id == current_user.id
   end
 
   def edit
