3a78562cd8d25763f63466eacf2e69ed6cac7995
SC-shunemuk <go.shun.ku@gmail.com>
2020-11-27 16:41:58 +0900

    コントローラーの整理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: :edit
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all
   end
@@ -19,16 +19,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    redirect_to action: :index unless @item.user_id == current_user.id
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
@@ -41,8 +39,8 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :content, :price, :image, :category_id, :status_id, :bear_price_id, :shipping_address_id, :shipping_day_id).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    item = Item.find(params[:id])
-    redirect_to action: :index unless item.user_id == current_user.id
+  def set_item
+    @item = Item.find(params[:id])
   end
+
 end
