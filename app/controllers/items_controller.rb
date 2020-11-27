7bcf939810b78f27cd27f62c98faf9d78f3cfd83
SC-shunemuk <go.shun.ku@gmail.com>
2020-11-27 19:00:40 +0900

    条件分岐の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,8 +22,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy 
-    redirect_to root_path if @item.user_id == current_user.id
+    @item.destroy if @item.user_id == current_user.id
+    redirect_to root_path 
   end
 
   def edit
