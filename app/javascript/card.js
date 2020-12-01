534f59f9093655b11b0f57e1eac667ac58402a4c
SC-shunemuk <go.shun.ku@gmail.com>
2020-12-01 16:52:17 +0900

    トークン化の処理

---
+++ app/javascript/card.js
@@ -0,0 +1,27 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_cd11fe3e3f3488d82e72ae1a")
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("number"),
+      cvc: formData.get("cvc"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+
+  });
+};
+
+window.addEventListener("load", pay);
